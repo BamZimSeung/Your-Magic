e3338d9ea61d2541a0061736e95086467b619ef6
RAPA08-PC\PC <uuobin10@gmail.com>
2017-07-26 14:28:04 +0900

    PM2:27 sourcetree s;

---
+++ Assets/YM_WB/WB_Scripts/WB_SpaceCtnl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WB_SpaceCtnl : MonoBehaviour {
+    public float delayScene = 5;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("DelayScene");
+	}
+	
+	IEnumerator DelayScene()
+    {
+        yield return new WaitForSeconds(delayScene);
+        SceneManager.LoadScene("WB_Scene1");
+    }
+}
