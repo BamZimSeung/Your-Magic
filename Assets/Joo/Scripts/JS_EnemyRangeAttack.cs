d71a35402af0b23534eca27f43772a81f64cd8eb
Ju Sung Park <pkjs91@naver.com>
2017-08-15 19:19:11 +0900

    적 원거리 공격 이펙트 및 포물선 공격 스크립트

---
+++ Assets/Joo/Scripts/JS_EnemyRangeAttack.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JS_EnemyRangeAttack : MonoBehaviour {
+
+    // 목표물
+    public Transform target;
+
+    // 방향
+    public Vector3 dir;
+
+    // 속도
+    public float speed;
+
+    // 포물선을 그리는지 여부
+    public bool isParabola;
+
+    // 포물선 최고점 높이
+    public float parabolaHeight;
+
+    // 처음 거리
+    float firstDist;
+
+    // 현재 거리
+    float tempDist;
+
+	void Start () {
+        firstDist = Vector3.Distance(target.position.x*Vector3.right + target.position.z*Vector3.forward, transform.position.x* Vector3.right + transform.position.z*Vector3.forward);
+	}
+	
+	void Update () {
+        SetPos();
+	}
+
+    void SetPos()
+    {
+        float upPos;
+
+        if (isParabola)
+        {
+            // y축이 0인 평면상의 목표물과의 거리
+            tempDist = Vector3.Distance(target.position.x * Vector3.right + target.position.z * Vector3.forward, transform.position.x * Vector3.right + transform.position.z * Vector3.forward);
+            // y = -4*H*(x - 0.5)^2 + H
+            upPos = -4 * parabolaHeight *(tempDist / firstDist - 0.5f) * (tempDist / firstDist - 0.5f) + parabolaHeight;
+
+            dir = (((target.position - target.position.y * Vector3.up) - (transform.position - transform.position.y * Vector3.up)).normalized * speed) * Time.deltaTime;
+        }
+        else
+        {
+            upPos = 0;
+         
+            dir = (target.position - transform.position).normalized * speed * Time.deltaTime;
+        }
+
+        transform.position += dir;
+        transform.position = new Vector3(transform.position.x, upPos, transform.position.z);
+    }
+}
