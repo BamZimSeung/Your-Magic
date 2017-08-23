b57946eee9dc0532ae03473bab56b49b3eb3afa5
Joo <pkjs91@naver.com>
2017-08-23 16:55:34 +0900

    UI

---
+++ Assets/Joo/Scripts/JS_UIView.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JS_UIView : MonoBehaviour {
+
+    public GameObject crosshair;
+    public float value;
+    Vector3 originScale;
+
+    void Start()
+    {
+        originScale = crosshair.transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Ray ray = new Ray(transform.position, transform.forward);
+
+        RaycastHit hitinfo;
+
+        int layerMask = 1 << LayerMask.NameToLayer("UI");
+
+        layerMask = ~layerMask;
+
+        if (Physics.Raycast(ray, out hitinfo, 1000, layerMask))
+        {
+            //크로스헤어의 위치 = 레이가 부딪힌 곳
+            crosshair.transform.position = hitinfo.point;
+
+            //크로스헤어의 크기를 원래 크기 * 거리 * 보정값
+            crosshair.transform.localScale = originScale * hitinfo.distance * value;
+        }
+        else
+        {
+            crosshair.transform.localPosition = Vector3.forward * 100;
+            crosshair.transform.localScale = originScale * 100 * value;
+        }
+    }
+}
